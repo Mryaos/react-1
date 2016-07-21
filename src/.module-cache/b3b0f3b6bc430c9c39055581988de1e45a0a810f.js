52c4ebf1e3c8686ed75c6c8fa81893af3f7841de
maxycode <maxy61209@gmail.com>
2016-07-21 10:34:02 +0800

    some edit

---
+++ src/.module-cache/b3b0f3b6bc430c9c39055581988de1e45a0a810f.js
@@ -0,0 +1,50 @@
+// Ajax组件封装
+// json(url, method, data, success, headers)
+
+function ajax(json) {
+    var xhr = null,
+    method = json.method || 'get',
+    data = json.data || null,
+    success = json.success || '',
+    headers = json.headers || null,
+    url = json.url,
+    async = json.async || true;
+
+    try {
+        xhr = new XMLHttpRequest();
+    } catch(e) {
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    if(method === 'get' && data) {
+        data = encodeURI(data);
+        url += url.indexOf('?') == -1 ? '?' + data : '&&' + data;
+    }
+
+    xhr.open(method, url, async);
+
+    if(headers) {
+        for(var i in headers) {
+            if(headers.hasOwnProperty(i)) {
+                xhr.setRequestHeader(i, headers[i]);
+            }
+        }
+    }
+
+    if(method === 'get') {
+        xhr.send();
+    } else {
+        xhr.setRequestHeader('content-type', 'application/json');
+        xhr.send(JSON.stringify(data));
+    }
+
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState === 4) {
+            if(xhr.status >= 200 && xhr.status < 300 || xhr.status == 304) {
+                success && success(xhr.responseText);
+            } else {
+                alert('Err: ' + xhr.status);
+            }
+        }
+    };
+}
